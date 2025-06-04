87b27555ac3f39fc4eda4f97c4811aa7067c65a4
Subzero710 <130773425+Subzero710@users.noreply.github.com>
2025-06-04 23:53:20 +0200

    Partie Facultative 1 terminée

--- main.py
+++ main.py
@@ -16,8 +16,12 @@ sys.path.insert(0, dist_dir)
 import simulateur
 
 SimulateurTraitement = simulateur.SimulateurTraitement
-
-def exoObligatoireMono(simulateur):
+def safeDiv(a,b):
+    if b == 0:
+        return 0  #on retourne 0 en cas de division 0/0 (le nominateur ne pourra jamais superieur au denominateur ici car ce seront des proportions)
+    else:
+        return a/b
+def exoObligatoireMono(simulateur, nb_patients):
     t = ['A','B','C','D','E']
     success_tracking = []   #nombre de succes totaux à l'instant T (1000 instants)
     total_success = 0
@@ -25,7 +29,7 @@ def exoObligatoireMono(simulateur):
     nb_A , nb_B, nb_C, nb_D, nb_E= 0,0,0,0,0
     s_A, s_B, s_C, s_D, s_E = 0, 0, 0, 0, 0
     proportionality_tracking = []   #on track la distribution à chaque instant des tests effectués (1000 instants/iterations)
-    for i in range(1000):
+    for i in range(nb_patients):
         traitement_choisi = t[randint(0,4)]
         if simulateur.administrer_traitement(traitement_choisi):
                 total_success+=1
@@ -57,21 +61,21 @@ def exoObligatoireMono(simulateur):
                 nb_E += 1
         it+=1
         success_tracking.append(total_success) #on suit l'evolution du nombre de succes
-        it_prop = [nb_A/it, nb_B/it, nb_C/it, nb_D/it, nb_E/it] #la distribution des tests en % effectués jusque à cette iteration
+        it_prop = [safeDiv(nb_A,it), safeDiv(nb_B,it), safeDiv(nb_C,it), safeDiv(nb_D,it), safeDiv(nb_E,it)] #la distribution des tests en % effectués jusque à cette iteration
         proportionality_tracking.append(it_prop)
 
-    success_rates = [(s_A/nb_A),(s_B/nb_B),(s_C/nb_C),(s_D/nb_D),(s_E/nb_E)] #taux de reussite de chaque test
+    success_rates = [safeDiv(s_A,nb_A),safeDiv(s_B,nb_B),safeDiv(s_C,nb_C),safeDiv(s_D,nb_D),safeDiv(s_E,nb_E)] #taux de reussite de chaque test
 
     return total_success,success_tracking,success_rates,proportionality_tracking
 
-def exoObligatoireMoyenne(simulateur): #on applique un raisonnement "en Moyenne"
+def exoObligatoireMoyenne(simulateur,nb_patients): #on applique un raisonnement "en Moyenne"
     all_total_success = []
     all_success_tracking = []
     all_success_rates = []
     all_proportions = []
 
     for loop in range(1000):
-        total, success, rates, proportions = exoObligatoireMono(simulateur)
+        total, success, rates, proportions = exoObligatoireMono(simulateur,nb_patients)
         all_total_success.append(total)
         all_success_tracking.append(success)
         all_success_rates.append(rates)
@@ -83,6 +87,84 @@ def exoObligatoireMoyenne(simulateur): #on applique un raisonnement "en Moyenne"
     mean_proportions = np.mean(all_proportions, axis = 0)
 
     return mean_total_success,mean_success_tracking,mean_success_rates,mean_proportions
+
+def exoFacultatif1Mono(simulateur,nb_patients):
+    t = ['A', 'B', 'C', 'D', 'E']
+    testingSample = randint(1,300)
+    it = testingSample
+    total_success, success_tracking, success_rates, proportionality_tracking = exoObligatoireMono(simulateur,testingSample)
+    nb_A, nb_B, nb_C, nb_D, nb_E = proportionality_tracking[it-1][0]*it, proportionality_tracking[it-1][1]*it, proportionality_tracking[it-1][2]*it, proportionality_tracking[it-1][3]*it, proportionality_tracking[it-1][4]*it  #on reconstruit le nombre de chacun des essais en inversant le calcul
+    s_A, s_B, s_C, s_D, s_E = success_rates[0]*nb_A, success_rates[1]*nb_B, success_rates[2]*nb_C, success_rates[3]*nb_D, success_rates[4]*nb_E
+    best = pickTheBest(success_rates)
+    for i in range(nb_patients-testingSample):
+        traitement_choisi = t[best]
+        if simulateur.administrer_traitement(traitement_choisi):
+                total_success+=1
+                if traitement_choisi == 'A':
+                    nb_A += 1
+                    s_A +=1
+                elif traitement_choisi == 'B':
+                    nb_B += 1
+                    s_B +=1
+                elif traitement_choisi == 'C':
+                    nb_C += 1
+                    s_C +=1
+                elif traitement_choisi == 'D':
+                    nb_D += 1
+                    s_D +=1
+                elif traitement_choisi == 'E':
+                    nb_E += 1
+                    s_E +=1
+        else:
+            if traitement_choisi == 'A':
+                nb_A += 1
+            elif traitement_choisi == 'B':
+                nb_B += 1
+            elif traitement_choisi == 'C':
+                nb_C += 1
+            elif traitement_choisi == 'D':
+                nb_D += 1
+            elif traitement_choisi == 'E':
+                nb_E += 1
+        it+=1
+        success_tracking.append(total_success)
+        it_prop = [safeDiv(nb_A,it), safeDiv(nb_B,it), safeDiv(nb_C,it), safeDiv(nb_D,it), safeDiv(nb_E,it)]
+        proportionality_tracking.append(it_prop)
+        success_rates = [safeDiv(s_A,nb_A),safeDiv(s_B,nb_B),safeDiv(s_C,nb_C),safeDiv(s_D,nb_D),safeDiv(s_E,nb_E)]
+        best = pickTheBest(success_rates)
+
+    return total_success, success_tracking, success_rates, proportionality_tracking
+
+def exoFacultatif1Moyenne(simulateur,nb_patients):
+    all_total_success = []
+    all_success_tracking = []
+    all_success_rates = []
+    all_proportions = []
+
+    for loop in range(1000):
+        total, success, rates, proportions = exoFacultatif1Mono(simulateur, nb_patients)
+        all_total_success.append(total)
+        all_success_tracking.append(success)
+        all_success_rates.append(rates)
+        all_proportions.append(proportions)
+
+    mean_total_success = np.mean(all_total_success)
+    mean_success_tracking = np.mean(all_success_tracking, axis=0)
+    mean_success_rates = np.mean(all_success_rates, axis=0)
+    mean_proportions = np.mean(all_proportions, axis=0)
+
+    return mean_total_success, mean_success_tracking, mean_success_rates, mean_proportions
+
+def pickTheBest(contenders):
+    best_nb = 0
+    best = 0
+    pos = -1
+    for c in contenders:
+        pos +=1
+        if best<c:
+            best = c
+            best_nb = pos
+    return best_nb
 
 if __name__ == "__main__":
     simulator = SimulateurTraitement()
@@ -96,8 +178,9 @@ if __name__ == "__main__":
             print(f"Traitement {t}: {'Succès' if resultat else 'Échec'}")
         except ValueError as e:
             print(e)
-    '''
+    
     mean_total_success,mean_success_tracking,mean_success_rates,mean_proportions = exoObligatoireMoyenne(simulator)
     print("exoObligatoire OK")
+    '''
 
 
--- visalisation.py
+++ visalisation.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
-from main import SimulateurTraitement, exoObligatoireMoyenne
+from main import SimulateurTraitement, exoObligatoireMoyenne, exoFacultatif1Mono, exoFacultatif1Moyenne
+
 
 def plot_success_tracking(tracking):
     plt.plot(tracking)
-    plt.title("Succès cumulés moyens (Uniforme)")
+    plt.title("Succès cumulés moyens")
     plt.xlabel("Patients")
     plt.ylabel("Succès cumulés")
     plt.grid(True)
@@ -12,7 +13,7 @@ def plot_success_tracking(tracking):
 def plot_success_rates(rates):
     traitements = ['A', 'B', 'C', 'D', 'E']
     plt.bar(traitements, rates)
-    plt.title("Taux de succès moyen par traitement (Uniforme)")
+    plt.title("Taux de succès moyen par traitement")
     plt.ylabel("Taux de succès")
     plt.ylim(0, 1)
     plt.grid(axis="y")
@@ -22,7 +23,7 @@ def plot_proportions(proportions):
     traitements = ['A', 'B', 'C', 'D', 'E']
     for i in range(5):
         plt.plot([p[i] for p in proportions], label=traitements[i])
-    plt.title("Nombre cumulé moyen de traitements utilisés (Uniforme)")
+    plt.title("Nombre cumulé moyen de traitements utilisés")
     plt.xlabel("Patients")
     plt.ylabel("Utilisations cumulées")
     plt.legend()
@@ -31,9 +32,10 @@ def plot_proportions(proportions):
 
 if __name__ == "__main__":
     simulator = SimulateurTraitement()
-    mean_total_success, mean_success_tracking, mean_success_rates, mean_proportions = exoObligatoireMoyenne(simulator)
-
-    print(f"Succès total moyen : {mean_total_success:.2f}")
-    plot_success_tracking(mean_success_tracking)
-    plot_success_rates(mean_success_rates)
-    plot_proportions(mean_proportions)
+    #mean_total_success, mean_success_tracking, mean_success_rates, mean_proportions = exoObligatoireMoyenne(simulator,1000)
+    #total_success, success_tracking, success_rates, proportions = exoFacultatif1Mono(simulator,1000)
+    total_success, success_tracking, success_rates, proportions = exoFacultatif1Moyenne(simulator,1000)
+    print(f"Succès total moyen : {total_success:.2f}")
+    plot_success_tracking(success_tracking)
+    plot_success_rates(success_rates)
+    plot_proportions(proportions)
